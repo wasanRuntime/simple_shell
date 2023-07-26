cc5b4a8f20a851d832c5e3b3a7177f041a0dc555
wasanRuntime <andrewwasan@gmail.com>
2023-07-26 15:31:47 +0300

    Edited

--- _strings2.c
+++ _strings2.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * _strcpy - Copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest.
- * @dest: Copy source to this buffer.
- * @src: This is the source to copy.
- * Return: Copy of original source.
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i, len;
-
-	for (len = 0; src[len] != '\0'; len++)
-		;
-
-	for (i = 0; i <= len; i++)
-		dest[i] = src[i];
-
-	return (dest);
-}
-
-/**
  * _putchar - Writes the character c to stdout.
  * @c: The character to print.
  * Return: On success - 1.
@@ -70,4 +48,27 @@ int c_atoi(char *s)
 		i++;
 	}
 	return (num);
+}
+
+
+/**
+ * _strcpy - Copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: Copy source to this buffer.
+ * @src: This is the source to copy.
+ * Return: Copy of original source.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	for (len = 0; src[len] != '\0'; len++)
+		;
+
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
